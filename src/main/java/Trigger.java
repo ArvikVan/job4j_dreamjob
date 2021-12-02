8e2f9fdc650ae1b90977a0af10b88f000693c756
ArvikVan <arvikvan@gmail.com>
2021-12-02 21:45:40 +0300

    create DJRepo

---
+++ src/main/java/Trigger.java
@@ -0,0 +1,10 @@
+/**
+ * @author ArvikV
+ * @version 1.0
+ * @since 02.12.2021
+ */
+public class Trigger {
+    public int someLogic() {
+        return 1;
+    }
+}
---
+++ src/test/java/TriggerTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author ArvikV
+ * @version 1.0
+ * @since 02.12.2021
+ */
+public class TriggerTest {
+
+    @Test
+    public void someLogic() {
+        Assert.assertEquals(1, new Trigger().someLogic());
+    }
+}
