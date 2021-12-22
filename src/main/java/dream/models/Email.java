d9b92554730569fc871a0bffc49039022d37266b
ArvikVan <arvikvan@gmail.com>
2021-12-23 00:52:30 +0300

    4.1. JSON формат. [#238554]

---
+++ src/main/java/dream/models/Email.java
@@ -0,0 +1,19 @@
+package dream.models;
+
+/**
+ * @author ArvikV
+ * @version 1.0
+ * @since 22.12.2021
+ * модель данных которую будем сериализовать/десериализовать
+ */
+public class Email {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+}
--- src/main/java/dream/servlet/GreetingServlet.java
+++ src/main/java/dream/servlet/GreetingServlet.java
@@ -1,11 +1,19 @@
 package dream.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dream.models.Email;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * @author ArvikV
@@ -13,14 +21,44 @@ import java.io.PrintWriter;
  * @since 21.12.2021
  */
 public class GreetingServlet extends HttpServlet {
+    private final List<Email> emails = new CopyOnWriteArrayList<>();
+
+    private static final Gson GSON = new GsonBuilder().create();
+    /**
+     * При обработке POST запроса производится десериализация модели. Далее объект сохраняется в список.
+     * @param req  запрос
+     * @param resp ответ
+     * @throws ServletException прокидываем сервлетЭксепшн
+     * @throws IOException прокидываем иоЭксепшн
+     */
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
-        resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Email email = GSON.fromJson(req.getReader(), Email.class);
+        emails.add(email);
+
+        resp.setContentType("application/json; charset=utf-8");
+        OutputStream output = resp.getOutputStream();
+        String json = GSON.toJson(email);
+        output.write(json.getBytes(StandardCharsets.UTF_8));
+        output.flush();
+        output.close();
+    }
+
+    /**
+     *При обработке GET запроса происходит сериализация списка добавленных почтовых адресов.
+     * @param req запрос
+     * @param resp ответ
+     * @throws ServletException прокидываем сервлетЭкспешн
+     * @throws IOException прокидываем иоЭксепшн
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json; charset=utf-8");
+        OutputStream output = resp.getOutputStream();
+        String json = GSON.toJson(emails);
         resp.setHeader("Access-Control-Allow-Origin", "*");
-        PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Nice to meet you, " + name);
-        writer.flush();
+        output.write(json.getBytes(StandardCharsets.UTF_8));
+        output.flush();
+        output.close();
     }
 }
